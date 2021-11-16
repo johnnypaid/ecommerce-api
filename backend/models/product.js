8224fa5e3dee979d7040bff4ca2e510f78976b0e
Lester Jainar <40604063+johnnypaid@users.noreply.github.com>
2021-11-16 13:05:16 +0800

    Product model and route update.

--- backend/models/product.js
+++ backend/models/product.js
@@ -58,6 +58,14 @@ const productSchema = new mongoose.Schema({
     }
 });
 
+productSchema.virtual('id').get(function() {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals: true
+});
+
 const Product = mongoose.model('Product', productSchema);
 
 module.exports.Product = Product;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,12 +7,26 @@ const { Category } = require('../models/category');
 const api = process.env.API_URL
 
 router.get('/', async (req, res) => {
-    const result = await Product.find().populate('category');
+    try {
+        // query params for category filtering
+        // api/v1/products?categories=618f97dbada7be425391783e,61923c4200299030d324643d
+        let filter = {};
 
-    if(!result) return res.status(500).json({success: false, message: 'Oops no products found!'});
+        if(req.query.categories) {
+            filter = {category: req.query.categories.split(',')}
+        }
+        // end filter
 
-    res.send(result);
-});
+        const result = await Product.find(filter).populate('category');
+
+        if(!result) return res.status(500).json({success: false, message: 'Oops no products found!'});
+
+        res.send(result);
+        
+    } catch (err) {
+        return res.status(400).json({success: false,error: err.message});
+    }
+})
 
 router.get('/:id', async (req, res) => {
     try {
@@ -24,7 +38,7 @@ router.get('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json({success: false, error: err});
     }
-});
+})
 
 router.post('/', async (req, res) => {
     let searchCategory = await Category.findById(req.body.category);
@@ -54,7 +68,7 @@ router.post('/', async (req, res) => {
     } catch (err) {
         return res.status(500).json({success: false, error: err.message})
     }
-});
+})
 
 router.put('/:id', async (req, res) => {
     if(!mongoose.isValidObjectId(req.params.id)) {
@@ -89,7 +103,7 @@ router.put('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json({success: false, error: err});
     }
-});
+})
 
 router.delete('/:id', async (req, res) => {
     if(!mongoose.isValidObjectId(req.params.id)) {
@@ -104,6 +118,49 @@ router.delete('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json({success: false, error: err})
     }
-});
+})
+
+// statisctics - product count
+router.get('/get/count', async (req, res) => {
+    try {
+        const productCount = await Product.countDocuments({});
+
+        if(!productCount) {
+            return res.status(500).json(
+                {
+                    success: false, 
+                    message: 'Cannot find any product.'
+                });
+        } 
+
+        res.send({count: productCount});
+
+    } catch (err) {
+        res.status(500).json({success: false, error: err.message})
+    }
+    
+})
+
+// statisctics - isFeatured product
+router.get('/get/featured/:count', async (req, res) => {
+    try {
+        const count = req.params.count ? req.params.count : 0;
+        const featuredProduct = await Product.find({isFeatured: true}).limit(+count);
+
+        if(!featuredProduct) {
+            return res.status(500).json(
+                {
+                    success: false, 
+                    message: 'No featured product.'
+                });
+        } 
+
+        res.send({success: true, data: featuredProduct});
+
+    } catch (err) {
+        res.status(500).json({success: false, error: err.message})
+    }
+    
+})
 
 module.exports = router;
